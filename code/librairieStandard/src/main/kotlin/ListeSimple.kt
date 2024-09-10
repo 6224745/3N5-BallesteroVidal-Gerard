ae9c825b237be48e6270bf7a01c88b758c520d6d
6224745 <6224745@cegepmontpetit.ca>
2024-09-10 10:22:57 -0400

    Exercice ListeSimple.kt

---
+++ code/librairieStandard/src/main/kotlin/ListeSimple.kt
@@ -0,0 +1,15 @@
+package org.ballestero
+
+fun main(){
+    println(repete(4, 2))
+}
+fun repete(n: Int, nbFois: Int): List<Int>{
+    val resultList = mutableListOf<Int>()
+
+    for (i in 1..n) {
+        repeat(nbFois) {
+            resultList.add(i)
+        }
+    }
+    return resultList
+}
