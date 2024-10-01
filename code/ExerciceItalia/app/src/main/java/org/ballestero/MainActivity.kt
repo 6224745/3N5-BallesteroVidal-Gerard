072cc4bee6d016dc551f79b5cd7f8f401e9a18ac
6224745 <6224745@cegepmontpetit.ca>
2024-10-01 09:34:58 -0400

    Exercice Italie

---
+++ code/ExerciceItalia/app/src/main/java/org/ballestero/MainActivity.kt
@@ -0,0 +1,20 @@
+package org.ballestero
+
+import android.os.Bundle
+import android.util.Log
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import  org.ballestero.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private  lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        Log.i("ALLO", "Yooooooo ")
+    }
+}
